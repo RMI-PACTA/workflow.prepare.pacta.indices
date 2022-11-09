b45d58c467cdfd105b48186d44ea8ee38bf41f5e
Jackson Hoffart <jackson.hoffart@gmail.com>
2022-11-09 09:21:55 +0100

    Add `prepare_pacta_indices.R` script (#1)
    * add prepare_indices script
    
    * rename script

---
+++ prepare_pacta_indices.R
@@ -0,0 +1,139 @@
+# necessary packages -----------------------------------------------------------
+
+suppressPackageStartupMessages({
+  library(cli)
+  library(dplyr)
+  library(fs)
+  library(purrr)
+  library(readr)
+  library(yaml)
+})
+
+
+# paths ------------------------------------------------------------------------
+
+working_dir <- here::here("working_dir")
+
+input_dir <- "../pacta-data/2021Q4"
+output_dir <- input_dir
+
+
+# options ----------------------------------------------------------------------
+
+pacta_directories <- c(
+  "00_Log_Files",
+  "10_Parameter_File",
+  "20_Raw_Inputs",
+  "30_Processed_Inputs",
+  "40_Results",
+  "50_Outputs"
+)
+
+holdings_date <- "2021Q4"
+
+
+# load indices data -------------------------------------------------------
+
+ishares_indices_bonds <- readRDS(file.path(input_dir, "ishares_indices_bonds.rds"))
+ishares_indices_equity <- readRDS(file.path(input_dir, "ishares_indices_equity.rds"))
+
+ishares_indices <- bind_rows(ishares_indices_bonds, ishares_indices_equity)
+
+
+# -------------------------------------------------------------------------
+
+temporary_directory <- tempdir()
+
+portfolio_names <- unique(ishares_indices$portfolio_name)
+
+for (portfolio_name in portfolio_names) {
+
+  fs::dir_delete(working_dir)
+  fs::dir_create(file.path(working_dir, pacta_directories))
+
+  portfolio <-
+    ishares_indices %>%
+    filter(portfolio_name == .env$portfolio_name)
+
+  investor_name <- unique(portfolio$investor_name)
+
+  if (length(investor_name) != 1L) {
+    stop("Please ensure that each index dataset contains a single unique value for `investor_name`")
+  }
+
+  config_list <-
+    list(
+      default = list(
+        parameters = list(
+          investor_name = investor_name,
+          portfolio_name = portfolio_name,
+          language = "EN",
+          project_code = investor_name,
+          holdings_date = holdings_date
+        )
+      )
+    )
+
+  yaml::write_yaml(
+    config_list,
+    file = file.path(
+      working_dir,
+      "10_Parameter_File",
+      paste0(portfolio_name, "_PortfolioParameters.yml")
+    )
+  )
+
+  write_csv(
+    portfolio,
+    file.path(working_dir, "20_Raw_Inputs", paste0(portfolio_name,".csv"))
+  )
+
+  portfolio_name_ref_all <- portfolio_name
+
+  cli::cli_alert_info("running PACTA on: {.emph {portfolio_name}}")
+  source("web_tool_script_1.R", local = TRUE)
+  source("web_tool_script_2.R", local = TRUE)
+
+  eq_result <- file.path(working_dir, "40_Results", portfolio_name, "Equity_results_portfolio.rds")
+  bond_result <- file.path(working_dir, "40_Results", portfolio_name, "Bonds_results_portfolio.rds")
+
+  eq_out <- file.path(temporary_directory, paste0(portfolio_name, "_", basename(eq_result)))
+  bond_out <- file.path(temporary_directory, paste0(portfolio_name, "_", basename(bond_result)))
+
+  if (file.exists(eq_result)) { file.copy(eq_result, eq_out) }
+  if (file.exists(bond_result)) { file.copy(bond_result, bond_out) }
+}
+
+
+# -------------------------------------------------------------------------
+
+output_files <- list.files(
+  temporary_directory,
+  pattern = "[.]rds$",
+  full.names = TRUE
+  )
+
+combined <-
+  output_files %>%
+  map_dfr(readRDS)
+
+unlink(output_files)
+
+combined <-
+  combined %>%
+  mutate(portfolio_name = case_when(
+    grepl("S&P", portfolio_name) ~ "iShares Core S&P 500 ETF",
+    grepl("MSCI World", portfolio_name) ~ "iShares MSCI World ETF",
+    grepl("MSCI Emerging Markets", portfolio_name) ~ "iShares MSCI Emerging Markets ETF",
+    grepl("Global Corp Bond", portfolio_name) ~ "iShares Global Corp Bond UCITS ETF",
+    grepl("MSCI ACWI", portfolio_name) ~ "iShares MSCI ACWI ETF",
+    TRUE ~ portfolio_name
+  ))
+
+combined %>%
+  filter(!grepl("Global Corp Bond", portfolio_name)) %>%
+  saveRDS(file.path(output_dir, "Indices_equity_portfolio.rds"))
+
+combined %>%
+  filter(grepl("Global Corp Bond", portfolio_name)) %>%
+  saveRDS(file.path(output_dir, "Indices_bonds_portfolio.rds"))
